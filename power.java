ea49f926c7159c1b46846a953df395a0d2e9b433
poorni1234 <47376824+poorni1234@users.noreply.github.com>
2019-03-08 01:43:40 -0800

    Create power.java

---
+++ power.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Excercise
+{
+public static void main(String args[])
+{
+Scanner nc=new Scanner(System.in);
+System.out.println("Enter value= ");
+int value=nc.nextInt();
+System.out.println("Enter Power value=");
+int power=nc.nextInt();
+System.out.println("OUTPUT="+(value^power));
+}
+}
